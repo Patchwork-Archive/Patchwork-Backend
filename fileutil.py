3a76218805d30cab4031fb3ec49623f1cb484fcb
Pinapelz <donaldshan1@outlook.com>
2023-07-02 02:15:58 -0700

    refactored some logic regarding replacing domain name

--- fileutil.py
+++ fileutil.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 
 def read_config(file_path: str):
     """
@@ -21,3 +22,14 @@ def check_file_exists(file_path: str):
         return True
     except FileNotFoundError:
         return False
+
+def read_site_config(file_path: str):
+    """
+    Reads a config file and returns a dictionary of the config
+    :param: file_path: str
+    :return: dict
+    """
+    file = open(file_path, "r")
+    data = file.read()
+    file.close()
+    return json.loads(data)
--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import fileutil
 from sql_handler import SQLHandler
 from flask_cors import CORS
@@ -9,8 +9,11 @@ CORS(app)
 
 if fileutil.check_file_exists("config.ini"):
     CONFIG = fileutil.read_config("config.ini")
+    SITE_CONFIG = fileutil.read_site_config("site_config.json")
 else:
     CONFIG = fileutil.read_config("/home/pinapelz/cover_viewer/config.ini")
+    SITE_CONFIG = fileutil.read_site_config("/home/pinapelz/cover_viewer/site_config.json")
+
 
 
 def create_database_connection():
@@ -29,22 +32,12 @@ def create_database_connection():
 
 @app.route("/")
 def random_video_player():
-    return render_template("random_video.html",
-                           domain=CONFIG.get("site", "domain"),
-                           )
-
-@app.route("/video/<video_id>")
-def specific_video(video_id):
-    server = create_database_connection()
-    if server.check_row_exists(table_name="songs", column_name="video_id", value=video_id):
-        data = server.get_query_result(f"SELECT * FROM songs WHERE video_id = '{video_id}'")
-        return render_template(
-            "specific_video.html",
-            video_id=data[0][0],
-            domain=CONFIG.get("site", "domain"),
+    return render_template(
+        "random_video.html",
+        domain=SITE_CONFIG["domain"],
+        cdn=SITE_CONFIG["cdn"],
+        humbnails_domain=SITE_CONFIG["thumbnails_domain"],
         )
-    server.close_connection()
-    return render_template("404.html")
 
 @app.route("/watch")
 def watch_video():
@@ -56,11 +49,11 @@ def watch_video():
         return render_template(
             "specific_video.html",
             video_id=data[0][0],
-            title=data[0][1],
-            channel_name=data[0][2],
-            published_date=data[0][4],
-            description=data[0][5]
+            domain=SITE_CONFIG["domain"],
+            cdn=SITE_CONFIG["cdn"],
+            humbnails_domain=SITE_CONFIG["thumbnails_domain"],
         )
+    server.c
     server.close_connection()
     return render_template("404.html")
 
@@ -93,6 +86,12 @@ def api_get_random_video():
     dict_data["description"] = data[0][5]
     server.close_connection()
     return dict_data
+
+@app.route("/api/stats")
+def api_get_stats():
+    server = create_database_connection()
+    song_count = server.get_query_result("SELECT COUNT(video_id) FROM songs")
+    return jsonify({"song_count": song_count[0][0]})
 
 if __name__ == "__main__":
     app.run(debug=True)
