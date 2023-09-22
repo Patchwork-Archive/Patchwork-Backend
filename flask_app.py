a4212560d87da2038ff31717e8fdc60bc0fa19d2
Pinapelz <donaldshan1@outlook.com>
2023-09-22 01:37:47 -0700

    explicitly create channel video API endpoint

--- flask_app.py
+++ flask_app.py
@@ -141,6 +141,20 @@ def channel_page(channel_id):
                            thumbnails_domain=SITE_CONFIG["thumbnails_domain"],
                            channel_name=channel_name,)
 
+@app.route("/api/channel/<channel_id>")
+def api_get_channel_videos(channel_id):
+    server = create_database_connection()
+    page_number = request.args.get('page') if request.args.get('page') is not None else 0
+    start_range = int(page_number) * 9
+    end_range = start_range + 9
+    data = server.get_query_result(f"SELECT * FROM songs WHERE channel_id = '{channel_id}' ORDER BY upload_date DESC LIMIT {str(start_range)}, {str(end_range)}")
+    videos = [{"video_id": video[0], "title": video[1], "channel_name": video[2], "channel_id": video[3], "upload_date": video[4], "description": video[5]} for video in data]
+    if len(videos) == 0:
+        return render_template("search_no_result.html", search_terms="No videos found")
+    server.close_connection()
+    return jsonify(videos)
+
+
 @app.route("/status")
 def status_page():
     database_status = "Online"
@@ -177,7 +191,7 @@ def api_search_query():
     server.close_connection()
     search_result = [{"video_id": video[0], "title": video[1], "channel_name": video[2], "channel_id": video[3], "upload_date": video[4], "description": video[5]} for video in data]
     if len(search_result) == 0:
-        return jsonify({"error": "No results found"})
+        return jsonify({"pages": 0, "results": []})
     return jsonify(search_result)
 
 @app.route("/api/video/<video_id>")
