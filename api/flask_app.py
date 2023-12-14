874987934772ae7913ef6435e364d02096b92cdd
Pinapelz <yukais@pinapelz.com>
2023-12-14 12:26:21 -0800

    Vercel deployment initial version

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,36 +1,317 @@
 from flask import Flask, render_template, request, jsonify, abort, redirect
-from database.sql_handler import SQLHandler
 from flask_cors import CORS
-from webapi.manual_storage_data import ManualStorageAPI
 import random
 import datetime
-import re
-import utils.fileutil as fileutil
 import requests
+import mysql.connector
+from mysql.connector import Error, errorcode
+from abc import ABC
+import requests
+import os
+
+
+    
+class SQLHandler:
+    def __init__(self):
+        self.connection = self._create_server_connection()
+        self._load_database(os.environ.get("DB_DATABASE"))
+    
+    def _create_ssh_server_connection(self) -> mysql.connector:
+        connection = None
+        try:
+            connection = mysql.connector.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("DB_DATABASE"),
+            user=os.environ.get("DB_USERNAME"),
+            password=os.environ.get("DB_PASSWORD"),
+            ssl_ca=os.environ.get("SSL_CERT"),
+            use_pure=True
+            )
+        except Error as err:
+            print(f"Error: '{err}'")
+        if connection is None:
+            print("Connection failed")
+            exit(1)
+        return connection
+
+    def get_connection(self):
+        return self.connection
+
+    def _create_database(self, cursor: str, database_name: str):
+        try:
+            cursor.execute(
+                f"CREATE DATABASE {database_name} DEFAULT CHARACTER SET 'utf8'")
+        except Error as err:
+            print(f"Failed creating database: {err}")
+            exit(1)
+
+    def _load_database(self, database_name: str):
+        try:
+            cursor = self.connection.cursor(buffered=True)
+        except Error as err:
+            print(f"Failed to load database: {err}")
+            exit(1)
+        try:
+            cursor.execute(f"USE {database_name}")
+            print(f"Database {database_name} loaded successfully")
+        except Error as err:
+            print(f"Database {database_name} does not exist")
+            if err.errno == errorcode.ER_BAD_DB_ERROR:
+                self._create_database(cursor, database_name)
+                print(f"Database {database_name} created successfully")
+                self.connection.database = database_name
+            else:
+                print(err)
+                exit(1)
+
+    def create_table(self, name: str, column: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"CREATE TABLE {name} ({column})")
+            print(f"Table {name} created successfully")
+        except Error as err:
+            print(err)
+
+    def insert_row(self, table_name: str, column: str, data: tuple):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            placeholders = ', '.join(['%s'] * len(data))
+            query = f"INSERT INTO {table_name} ({column}) VALUES ({placeholders})"
+            cursor.execute(query, data)
+            self.connection.commit()
+            print("Data Inserted:", data)
+        except Error as err:
+            print("Error inserting data")
+            print(err)
+            if err not in ("Duplicate entry", "Duplicate entry for key 'PRIMARY'"):
+                return False
+        return True
+
+    def close_connection(self):
+        if self.connection.is_connected():
+            if hasattr(self, '_tunnel'):
+                self._tunnel.stop()
+            self.connection.close()
+            print("MySQL connection is closed")
+
+
+    def clear_table(self, name: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"DELETE FROM {name}")
+            self.connection.commit()
+            print("Table cleared successfully")
+        except Error as err:
+            print("Error clearing table")
+            print(err)
+
+    def reset_auto_increment(self, name: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"ALTER TABLE {name} AUTO_INCREMENT = 1")
+            self.connection.commit()
+            print("Table reset successfully")
+        except Error as err:
+            print("Error resetting table")
+            print(err)
+
+    def copy_rows_to_new_table(self, name: str, new_name: str, column: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(
+                f"INSERT INTO {new_name} ({column}) SELECT {column} FROM {name}")
+            cursor.execute(
+                f"ALTER TABLE {new_name} MODIFY COLUMN id INT AUTO_INCREMENT")
+            self.connection.commit()
+            print("Rows copied successfully")
+        except Error as err:
+            print("Error copying rows")
+            print(err)
+
+    def drop_table(self, name: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"DROP TABLE {name}")
+            self.connection.commit()
+            print("Table dropped successfully")
+        except Error as err:
+            print("Error dropping table")
+            print(err)
+    
+    def check_row_exists(self, table_name: str, column_name: str, value: str):
+        """
+        Checks if a row exists in a table
+        """
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"SELECT * FROM {table_name} WHERE {column_name} = '{value}'")
+            result = cursor.fetchone()
+            if result:
+                return True
+            else:
+                return False
+        except Error as err:
+            print("Error checking row")
+            print(err)
+
+    def update_row(self, name: str, column_name: str, search_val: str, replace_col:str, new_value: str):
+        """
+        Updates a row in a table
+        """
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"UPDATE {name} SET {replace_col} = '{new_value}' WHERE {column_name} = '{search_val}'")
+            self.connection.commit()
+            print("Row updated successfully")
+        except Error as err:
+            print("Error updating row")
+            print(err)
+    
+    def execute_query(self, query: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+        except Error as err:
+            print("Error executing query")
+            print(err)
+    
+    def get_query_result(self, query: str):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+        except Error as err:
+            print("Error executing query")
+            print(err)
+
+    def delete_row(self, name: str, column: str, data: tuple):
+        cursor = self.connection.cursor()
+        try:
+            query = f"DELETE FROM {name} WHERE {column} = %s"
+            cursor.execute(query, data)
+            self.connection.commit()
+            print("Data Deleted:", data)
+        except Error as err:
+            print("Error deleting data")
+            print(err)
+            return False
+        return True
+    
+    def get_random_row(self,table_name: str, limit: int = 1):
+        cursor = self.connection.cursor(buffered=True)
+        try:
+            cursor.execute(f"SELECT * FROM {table_name} ORDER BY RAND() LIMIT {str(limit)}")
+            result = cursor.fetchall()
+            return result
+        except Error as err:
+            print("Error getting random row")
+            print(err)
+    
+    def search_video_row(self, table_name: str, keywords: list, limit: int = 1, offset: int = 0):
+        cursor = self.connection.cursor(buffered=True)
+        query = f"SELECT * FROM {table_name} WHERE 1=1"
+        keyword_conditions = [] 
+
+        for keyword in keywords:
+            keyword_condition = f"LOWER(title) LIKE %s"
+            formatted_keyword = f"%{keyword.lower()}%"  
+            keyword_conditions.append((keyword_condition, formatted_keyword))  
+        if keyword_conditions:
+            query += " AND " + " AND ".join([condition[0] for condition in keyword_conditions])
+
+        query += " LIMIT %s OFFSET %s"
+
+        try:
+            cursor.execute(query, ([condition[1] for condition in keyword_conditions] + [limit, offset]))
+            result = cursor.fetchall()
+            return result
+        except Error as err:
+            print("Error searching video row")
+            print(err)
+
+
+
+class WebAPI():
+    """
+    General class for interacting with web APIs
+    """
+
+    def __init__(self, api_key: str, base_url: str, headers: dict) -> None:
+        self.api_key = api_key
+        self._base_url = base_url
+        self._headers = headers
+
+    def _get_url(self, query: str) -> dict:
+        """
+        Downloads the URL and returns the result as a dict
+        param:
+            query: str - the query to be appended to the base URL
+        """
+        response = requests.get(query, headers=self._headers)
+        json_data = response.json()
+        return json_data
+
+
+class StorageAPI(ABC, WebAPI):
+    """
+    The data for this use case is stored on CloudFlare R2 Storage
+    However so long as all methods are implemented the class can be re-written to use any other API
+    """
+    def __init__(self, api_key: str, base_url: str, headers:dict) -> None:
+        super().__init__(api_key, base_url, headers)
+    
+    def get_storage_used(self) -> tuple[int,str]:
+        """
+        Returns the amount of storage used and the unit of measurement
+        """
+        pass
+
+    def get_number_of_files(self) -> int:
+        """
+        Returns the number of files stored
+        """
+        pass
+    
+class ManualStorageAPI(StorageAPI):
+    def __init__(self, server: SQLHandler) -> None:
+        super().__init__(None, None, None)
+        self.server = server
+
+    def get_storage_used(self) -> tuple[int,str]:
+        """
+        Returns the amount of storage used and the unit of measurement
+        """
+        print("Getting storage used")
+        if not self.server.check_row_exists("kv", "DATA", "video_bucket_size"):
+            print("Inserting row")
+            self.server.insert_row("kv", "DATA, REFERENCE", ("video_bucket_size", "0"))
+        print(self.server.get_query_result("SELECT REFERENCE FROM kv WHERE DATA = 'video_bucket_size'"))
+        storage_used = int(self.server.get_query_result("SELECT REFERENCE FROM kv WHERE DATA = 'video_bucket_size'")[0][0])
+
+        return storage_used
+
+    def get_number_of_files(self) -> int:
+        """
+        Returns the number of files stored
+        """
+        if not self.server.check_row_exists("kv", "DATA", "video_bucket_count"):
+            self.server.insert_row("kv", "DATA, REFERENCE", ("video_bucket_count", "0"))
+        obj_count = int(self.server.get_query_result("SELECT REFERENCE FROM kv WHERE DATA = 'video_bucket_count'")[0][0])
+        print(obj_count)
+        return obj_count
+
+
 
 app = Flask(__name__)
 CORS(app)
 current_radio_data = None
 
-if fileutil.check_file_exists("config.ini"):
-    CONFIG = fileutil.read_config("config.ini")
-    SITE_CONFIG = fileutil.read_site_config("site_config.json")
-else:
-    CONFIG = fileutil.read_config("/home/pinapelz/cover_viewer/config.ini")
-    SITE_CONFIG = fileutil.read_site_config("/home/pinapelz/cover_viewer/site_config.json")
 
 def create_database_connection():
-    hostname = CONFIG.get("database", "host")
-    user = CONFIG.get("database", "user")
-    password = CONFIG.get("database", "password")
-    database = CONFIG.get("database", "database")
-    ssh_host = CONFIG.get("database", "ssh_host")
-    ssh_username = CONFIG.get("database", "ssh_username")
-    ssh_password = CONFIG.get("database", "ssh_password")
-    remote_bind = CONFIG.get("database", "remote_bind")
-    if ssh_host.strip() == "" or ssh_username.strip() == "" or ssh_password.strip() == "":
-        return SQLHandler(hostname, user, password, database)
-    return SQLHandler(hostname, user, password, database, ssh_host, ssh_username, ssh_password, remote_bind)
+    return SQLHandler()
 
 
 def pick_featured_videos(max_videos: int):
@@ -93,8 +374,8 @@ def api_search_query():
     server = create_database_connection()
     search_terms = request.args.get('q')
     page = request.args.get('page') if request.args.get('page') is not None else 1
-    start_range = int(SITE_CONFIG["search_results_per_page"]) * (int(page) - 1)
-    data = server.search_video_row("songs", search_terms.split(), int(SITE_CONFIG["search_results_per_page"]), start_range)
+    start_range = int(os.environ.get("RESULTS_PER_PAGE")) * (int(page) - 1)
+    data = server.search_video_row("songs", search_terms.split(), int(os.environ.get("RESULTS_PER_PAGE")), start_range)
     server.close_connection()
     search_result = [{"video_id": video[0], "title": video[1], "channel_name": video[2], "channel_id": video[3], "upload_date": video[4], "description": video[5]} for video in data]
     if len(search_result) == 0:
@@ -322,7 +603,7 @@ def get_storage_status():
     """
     try:
         server = create_database_connection()
-        storage_api = ManualStorageAPI(CONFIG["storage"]["api_token"], CONFIG["storage"]["accountID"], CONFIG["storage"]["bucket_name"], server)
+        storage_api = ManualStorageAPI(server)
         number_of_files = int(server.get_query_result("SELECT COUNT(*) FROM songs")[0][0])
         storage_size = str(round(int(storage_api.get_storage_used())/ (1024 **3), 2))
         server.close_connection()
