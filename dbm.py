b325df5d73b940e1d4ac77c474c444d434304b15
Pinapelz <yukais@pinapelz.com>
2024-04-28 14:15:35 -0700

    update dbm.py to fit new schema

--- dbm.py
+++ dbm.py
@@ -1,9 +1,8 @@
 """
-Database Manager Module to be executed seperately for adding and removing auth tokens
+Database Manager Module to be executed separately for adding and removing auth tokens
 """
 from database.sql_handler import SQLHandler
 import argparse
-
 
 def create_database_connection():
     return SQLHandler()
@@ -20,16 +19,30 @@ def main():
         return
     print("Database connection established.\n")
     args = parser.parse_args()
+    server.create_table("archive_log", "id INTEGER PRIMARY KEY AUTO_INCREMENT, url TEXT NOT NULL, user VARCHAR(128) NOT NULL, status TEXT NOT NULL, timestamp TEXT")
     server.create_table("archive_queue", "id INTEGER PRIMARY KEY AUTO_INCREMENT, url TEXT, mode INTEGER")
-    server.create_table("archive_queue_auth", "id INTEGER PRIMARY KEY AUTO_INCREMENT, token TEXT")
-    server.create_table("archive_worker_auth", "id INTEGER PRIMARY KEY AUTO_INCREMENT, token TEXT")
-    server.create_table("archive_log", "id INTEGER PRIMARY KEY AUTO_INCREMENT, url TEXT, user TEXT, status TEXT, timestamp TEXT")
-    server.create_table("worker_status", "id INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT, token TEXT, status TEXT, timestamp TEXT")
+    server.create_table("archive_queue_auth", "id INTEGER PRIMARY KEY AUTO_INCREMENT, token VARCHAR(128)")
+    server.create_table("archive_worker_auth", "id INTEGER PRIMARY KEY AUTO_INCREMENT, token VARCHAR(128) NOT NULL")
+    server.create_table("worker_status", "id INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT, token VARCHAR(128), status TEXT, timestamp TEXT")
     server.create_table("kv", "DATA VARCHAR(255) NOT NULL PRIMARY KEY, REFERENCE VARCHAR(255)")
-    server.create_table("songs", "video_id VARCHAR(255) NOT NULL, title text, channel_name text, channel_id text, upload_date text, description text, id INTEGER PRIMARY KEY AUTO_INCREMENT")
-    server.create_table("romanized", "id INTEGER PRIMARY KEY AUTO_INCREMENT, video_id VARCHAR(255) NOT NULL, romanized_title text")
-    server.create_table("channels", "channel_id VARCHAR(255) PRIMARY KEY, channel_name TEXT NOT NULL, romanized_name TEXT, description TEXT")
-    
+    server.create_table("songs", "video_id VARCHAR(255) NOT NULL, title TEXT NOT NULL, channel_name TEXT NOT NULL, channel_id VARCHAR(255) NOT NULL, upload_date TEXT NOT NULL, description TEXT NOT NULL, id INTEGER PRIMARY KEY AUTO_INCREMENT")
+    server.create_table("romanized", "id INTEGER PRIMARY KEY AUTO_INCREMENT, video_id VARCHAR(255) NOT NULL, romanized_title TEXT NOT NULL")
+    server.create_table("channels", "channel_id VARCHAR(255) NOT NULL PRIMARY KEY, channel_name TEXT NOT NULL, romanized_name TEXT, description TEXT")
+
+    # TODO: Add foreign key constraints
+    """
+    server.add_constraint("channels", "FOREIGN KEY (channel_id) REFERENCES songs (channel_id)")
+    server.add_constraint("romanized", "FOREIGN KEY (video_id) REFERENCES songs (video_id) ON DELETE CASCADE")
+    server.add_constraint("worker_status", "FOREIGN KEY (token) REFERENCES archive_worker_auth (token)")
+    """
+
+    # TODO: Add indexes
+    """
+    server.create_index("archive_worker_auth", "token")
+    server.create_index("songs", "channel_id")
+    server.create_index("songs", "video_id")
+    """
+
     if args.addqueuetoken is not None:
         print("Adding queue token...")
         server.insert_row("archive_queue_auth", "token", (args.addqueuetoken,))
@@ -37,7 +50,6 @@ def main():
     if args.addworkertoken is not None:
         print("Adding worker token...")
         server.insert_row("archive_worker_auth", "token", (args.addworkertoken,))
-
 
 if __name__ == "__main__":
     main()
